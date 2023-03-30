d2f8f49f694aee5dcf487f38809b8fdb3eca3a43
Juan Serrano Soria <54719300+juan-serrano-soria@users.noreply.github.com>
2023-03-30 14:24:56 +0200

    Refactor firebase configuration

--- App.tsx
+++ App.tsx
@@ -1,24 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 import Navigation from './src/Navigation';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: ".",
-  authDomain: ".",
-  databaseURL: ".",
-  projectId: "",
-  storageBucket: ".",
-  messagingSenderId: ".",
-  appId: "."
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
 function App(): JSX.Element {
 

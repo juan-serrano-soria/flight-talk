e72c310636c9e15a737081b52c9c7d9b73b95755
Juan Serrano Soria <54719300+juan-serrano-soria@users.noreply.github.com>
2023-04-03 18:39:48 +0200

    Styled signup screen

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,18 +1,94 @@
-import { Text, View } from "react-native"
+import { Text, TextInput, View, StyleSheet, Button, Alert } from "react-native";
+import { useState } from "react";
 
 const SignUp = ({ navigation }) => {
 
   const signUp = () => {
+    if (password !== repeatPassword) {
+      Alert.alert("Error", "Passwords do not match");
+      return;
+    }
     navigation.navigate('Login');
   };
 
+  const cancel = () => {
+    navigation.navigate('Login');
+  };
+
+  const [user, setUser] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
+
   return (
-    <View style={{ flex: 1, backgroundColor: 'black', alignItems: 'center', padding: 10}}>
-      <Text style={{ padding: 10}}>Email</Text>
-      <Text style={{ padding: 10}}>Password</Text>
-      <Text style={{ padding: 10}} onPress={signUp}>Sign Up</Text>
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.title}>Sign Up</Text>
+      </View>
+      <View style={styles.inputContainer}>
+      <TextInput
+          style={styles.input}
+          onChangeText={setUser}
+          value={user}
+          placeholder="Username"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={setEmail}
+          value={email}
+          placeholder="Email"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={setPassword}
+          value={password}
+          placeholder="Password"
+          secureTextEntry={true}
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={setRepeatPassword}
+          value={repeatPassword}
+          placeholder="Repeat password"
+          secureTextEntry={true}
+        />
+      </View>
+      <View style={styles.button}>
+        <Button onPress={signUp} title="Sign Up"/>
+      </View>
+      <View style={styles.button}>
+        <Button onPress={cancel} title="Cancel" color="#cf142b"/>
+      </View>
     </View>
   )
 }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'black',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    title: {
+      fontSize: 40,
+      margin: 10,
+    },
+    inputContainer: {
+      margin: 10,
+      width: '70%',
+    },
+    input: {
+      height: 40,
+      margin: 5,
+      borderWidth: 1,
+      borderColor: 'white',
+      padding: 10,
+    },
+    button: {
+      width: "70%",
+      margin: 5,
+    }
+  });
 
 export default SignUp;
